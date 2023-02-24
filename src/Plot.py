59a48ebeae331dc549d4f05a7ea47cba44eaa240
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-02-24 10:08:32 +0700

    fixed plotting

--- src/Plot.py
+++ src/Plot.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
 
-# Plot given points in blue and the closest pair colored in red and draw a line between them in 3D
-def result_plot(points, closest_pair):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(*zip(*points))
-    ax.scatter(*closest_pair[0], color='red')
-    ax.scatter(*closest_pair[1], color='red')
-    ax.plot([closest_pair[0][0], closest_pair[1][0]], [closest_pair[0][1], closest_pair[1][1]], [closest_pair[0][2], closest_pair[1][2]], color='red')
-    plt.show()
+# Plot given points in blue and the closest pair colored in red and draw a line between them in given dimension
+def result_plot(points, closest_pair, dimension):
+    if dimension == 2:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(*zip(*points))
+        ax.scatter(*closest_pair[0], color='red')
+        ax.scatter(*closest_pair[1], color='red')
+        ax.plot([closest_pair[0][0], closest_pair[1][0]], [closest_pair[0][1], closest_pair[1][1]], color='red')
+        plt.show()
+
+    elif dimension == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(*zip(*points))
+        ax.scatter(*closest_pair[0], color='red')
+        ax.scatter(*closest_pair[1], color='red')
+        ax.plot([closest_pair[0][0], closest_pair[1][0]], [closest_pair[0][1], closest_pair[1][1]], [closest_pair[0][2], closest_pair[1][2]], color='red')
+        plt.show()
+    
+    else:
+        print('Plotting is only available in 2D and 3D')
