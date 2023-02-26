f216dc95217b2d8d53af3c7f9d7e6d2140158c39
ditramadia <ditrarizqaamadia@gmail.com>
2023-02-26 13:00:26 +0700

    feat: add splash screen

---
+++ src/Splash.py
@@ -0,0 +1,5 @@
+def dsiplaySplash():
+    print("\nTHE CLOSEST PAIR PROBLEM SOLVER")
+    print("\n              by               ")
+    print("\n Ditra Rizqa Amadia (13521019) ")
+    print("     Ahmad Nadil (13521025)    ")
--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 import numpy as np
+from Splash import *
 from System import *
 from ClosestPair import*
 from Plot import*
 from time import time
 import Euclidean as e
 
-n = int(input("Masukkan banyaknya titik: "))
+# Splash Screen
+dsiplaySplash()
+
+# Inputs
+n = int(input("\nMasukkan banyaknya titik: "))
 d = int(input("Masukkan dimensi: "))
 points = np.random.randint(-1000, 1000, (n, d))
 
+# Hardware Specification
 print("\n== HARDWARE SPECIFICATION ==========----------")
 displaySpecification()
 
+# Solution by Brute Force
 start = time()
 min = brute_force(points)
 end = time()
 print(f'Waktu eksekusi brute force: {"{:.2f}".format((end - start) * 1000)} ms')
 print(f'Closest distance brute force: {min}')
 
+# Solution by Divide and Conquer
 start = time()
 closest_pair = closest_pair(points)
 end = time()
