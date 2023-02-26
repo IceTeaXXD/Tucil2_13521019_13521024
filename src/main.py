b6721b1378635e0565667b12a2bde3b542548349
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-02-26 14:27:42 +0700

    fix: changed execution time in second

--- src/main.py
+++ src/main.py
@@ -23,13 +23,13 @@ print("\n== BRUTE FORCE ==========---------------------")
 start = time()
 min = brute_force(points)
 end = time()
-bf_extime = (end-start) * 1000
+bf_extime = (end-start)
 bf_euclidop = e.euclidCounter
 print('Closest pair of points:')
 print(f'Point 1 : \t\t{min[0]}')
 print(f'Point 2 : \t\t{min[1]}')
 print(f'Distance: \t\t{min[2]}')
-print(f'Execution time: \t{"{:.2f}".format(bf_extime)} ms')
+print(f'Execution time: \t{"{:.4f}".format(bf_extime)} s')
 print(f'Euclidean Distance Operations: {bf_euclidop}')
 
 # Solution by Divide and Conquer
@@ -37,13 +37,13 @@ print("\n== DIVIDE AND CONQUER ==========--------------")
 start = time()
 closest_pair = closest_pair(points)
 end = time()
-dc_extime = (end-start) * 1000
+dc_extime = (end-start)
 dc_euclidop = e.euclidCounter-bf_euclidop
 print('Closest pair of points:')
 print(f'Point 1 : \t\t{closest_pair[0]}')
 print(f'Point 2 : \t\t{closest_pair[1]}')
 print(f'Distance: \t\t{closest_pair[2]}')
-print(f'Execution time: \t{"{:.2f}".format(dc_extime)} ms')
+print(f'Execution time: \t{"{:.4f}".format(dc_extime)} s')
 print(f'Euclidean Distance Operations: {dc_euclidop}')
 
 
@@ -54,9 +54,9 @@ if(closest_pair[2] == min[2]):
 else:
     print("Solutions does not match")
 if(dc_extime < bf_extime):
-    print(f"Divide and conquer is faster by {'{:.2f}'.format(bf_extime-dc_extime)} ms")
+    print(f"Divide and conquer is faster by {'{:.4f}'.format(bf_extime-dc_extime)} s")
 else:
-    print(f"Brute force is faster by {'{:.2f}'.format(dc_extime-dc_extime)} ms")
+    print(f"Brute force is faster by {'{:.4f}'.format(dc_extime-bf_extime)} s")
 if(dc_euclidop < bf_euclidop):
     print(f"Divide and conquer used {bf_euclidop-dc_euclidop} less euclidian distance operations")
 else:
